8497bfd86c4d2bda6b26e7fba04ab5b9d8023584
Pranav Munot <52027465+PranavMunot@users.noreply.github.com>
2023-07-05 01:28:35 +0530

    Update index.js

--- SocketServer/index.js
+++ SocketServer/index.js
@@ -9,7 +9,7 @@ const users = new Set()
 
 const io = require('socket.io')(httpServer, {
     cors: {
-        origin: "http://localhost:3000",
+        origin: "*",
     },
     methods: ['GET', 'POST']
 })
